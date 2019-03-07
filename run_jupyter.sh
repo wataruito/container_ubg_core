e5b2460390680e80b25a5b29c87217fc463317f7
Wataru Ito <wataru.ito@gmail.com>
2019-03-07 17:02:47 -0500

    token is ubg

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jupyter notebook --ip=0.0.0.0 --no-browser --notebook-dir=$JUPYTER_START
+jupyter notebook --ip=0.0.0.0 --no-browser --notebook-dir=$JUPYTER_START --NotebookApp.token='ubg'
