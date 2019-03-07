a48cd4d51b68b5c5d887a85fad9b84267c2faa03
Wataru Ito <wataru.ito@gmail.com>
2019-03-07 17:03:30 -0500

    same things

--- run_jupyterlab.sh
+++ run_jupyterlab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jupyter lab --ip=0.0.0.0 --no-browser --notebook-dir=$USER_HOME/Dropbox/Jupyter --NotebookApp.token='ubg'
+jupyter lab --ip=0.0.0.0 --no-browser --notebook-dir=$JUPYTER_START --NotebookApp.token='ubg'
